f58ede29c41b96bd8098a795b30a6bee6d5b333f
timplifier <timplifier@gmail.com>
2024-05-26 00:34:50 +0600

    feat(settings.gradle.kts): added repositories for plugin and dependency management; enabled previews

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,22 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
 }
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "clean-architecture-mapper"
 include("clean-architecture-mapper-processor")
 include("workload")

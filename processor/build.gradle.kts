da3a603ff021a6a87a8ddae22bccf2a748589a71
timplifier <timplifier@gmail.com>
2024-05-26 00:46:07 +0600

    style(processor/build.gradle.kts): reordered dependencies

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.google.devtools.ksp)
     implementation(libs.squareup.kotlinpoet)
     implementation(libs.squareup.kotlinpoet.ksp)
-    implementation(libs.kotlinx.serialization.json)
 }

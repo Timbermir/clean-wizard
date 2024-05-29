4549a7e35e6d91f00f36f48b5f8ae712f096ec4e
timplifier <timplifier@gmail.com>
2024-05-30 00:31:11 +0600

    feat(workoad/Main.kt): cleaned

--- workload/src/main/kotlin/corp/tbm/cleanarchitecturemapper/workload/Main.kt
+++ workload/src/main/kotlin/corp/tbm/cleanarchitecturemapper/workload/Main.kt
@@ -1,30 +1,4 @@
 package corp.tbm.cleanarchitecturemapper.workload
 
-import corp.tbm.cleanarchitecturemapper.processor.foundation.annotations.BooleanEnum
-import corp.tbm.cleanarchitecturemapper.processor.foundation.annotations.DTO
-import corp.tbm.cleanarchitecturemapper.processor.foundation.annotations.IntEnum
-import corp.tbm.cleanarchitecturemapper.processor.foundation.annotations.StringEnum
-
 fun main() {
 }
-
-@DTO
-data class DebilDTO(
-    @IntEnum(["Daun", "Debil", "Kon4"], "debil", [1, 2, 3])
-    val debil: Int = 1,
-    @StringEnum(["slkdfjls", "dfkljgdjl"], "kon4", ["sosi", "4len"])
-    val debikk : String
-)
-
-@DTO
-data class Kon4DTO(
-    @StringEnum(["slkdfjls", "dfkljgdjl"], "kon4", ["sosi", "4len"])
-    val chlen: String
-)
-
-@DTO
-
-data class Kon4DsgldkfTO(
-    @BooleanEnum(["slkdfjls", "dfkljgdjl"], "kon4", [false, false])
-    val chelentatno: Boolean
-)

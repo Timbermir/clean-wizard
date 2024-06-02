7aa0089e87404fe37f41122a4151ef590e1b9531
timplifier <timplifier@gmail.com>
2024-06-03 03:45:39 +0600

    feat(foundation/universal/EnumType.kt): added more parameters

--- foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/EnumType.kt
+++ foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/EnumType.kt
@@ -1,14 +1,22 @@
 package corp.tbm.cleanarchitecturemapper.foundation.codegen.universal
 
 import corp.tbm.cleanarchitecturemapper.foundation.annotations.*
+import kotlin.reflect.KClass
 
-enum class EnumType(val annotation: String) {
-    STRING(StringEnum::class.qualifiedName.toString()),
-    BYTE(ByteEnum::class.qualifiedName.toString()),
-    SHORT(ShortEnum::class.qualifiedName.toString()),
-    INT(IntEnum::class.qualifiedName.toString()),
-    LONG(LongEnum::class.qualifiedName.toString()),
-    FLOAT(FloatEnum::class.qualifiedName.toString()),
-    DOUBLE(DoubleEnum::class.qualifiedName.toString()),
-    BOOLEAN(BooleanEnum::class.qualifiedName.toString());
+enum class EnumType(
+    val annotation: KClass<out Annotation>,
+    var enumName: String = "",
+    var parameterName: String = "",
+    val parameterValueSuffix: Char = ' ',
+    var enumEntries: ArrayList<String> = arrayListOf(),
+    var enumEntryValues: ArrayList<Any> = arrayListOf()
+) {
+    STRING(StringEnum::class),
+    BYTE(ByteEnum::class),
+    SHORT(ShortEnum::class),
+    INT(IntEnum::class),
+    LONG(LongEnum::class, parameterValueSuffix = 'L'),
+    FLOAT(FloatEnum::class, parameterValueSuffix = 'f'),
+    DOUBLE(DoubleEnum::class),
+    BOOLEAN(BooleanEnum::class);
 }

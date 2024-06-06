07701255e9a536af8f71e24f287f66455a9a893e
timplifier <timplifier@gmail.com>
2024-06-06 19:46:42 +0600

    refactor(foundation/codegen/universal/extensions/LoggerExtensions.kt): fixed

--- foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/extensions/LoggerExtensions.kt
+++
@@ -1,8 +0,0 @@
-package corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.extensions
-
-import com.google.devtools.ksp.processing.KSPLogger
-import com.google.devtools.ksp.symbol.KSNode
-
-fun KSPLogger.log(message: String = "CAP", symbol: KSNode? = null) {
-    warn(message, symbol)
-}

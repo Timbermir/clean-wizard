016892f10839646aec2de90dfa5e838bf5abd632
Timur Abdyraev <timplifier@gmail.com>
2024-06-07 19:57:52 +0600

    refactor(workload/Main.kt): cleaned

--- workload/src/main/kotlin/corp/tbm/cleanarchitecturemapper/workload/Main.kt
+++ workload/src/main/kotlin/corp/tbm/cleanarchitecturemapper/workload/Main.kt
@@ -1,18 +1,5 @@
 package corp.tbm.cleanarchitecturemapper.workload
 
-import corp.tbm.cleanarchitecturemapper.foundation.annotations.BooleanEnum
-import corp.tbm.cleanarchitecturemapper.foundation.annotations.DTO
-
 fun main() {
+
 }
-
-@DTO
-data class SosokDTOSchema(
-    val name123: String,
-    val debik: List<DebikDTOSchema>
-)
-
-@DTO(false)
-data class DebikDTOSchema(
-    val debik: String
-)

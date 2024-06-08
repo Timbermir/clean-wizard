b7f181f9f06b23394365a131a418c906e79246ee
timplifier <timplifier@gmail.com>
2024-06-08 23:15:52 +0600

    style(buildLogic/convention/foundation/plugins/*): moved

--- clean-wizard/src/main/kotlin/corp/tbm/cleanwizard/DTOMapper.kt
+++
@@ -1,9 +0,0 @@
-package corp.tbm.cleanwizard
-
-interface DTOMapper<MODEL> {
-    fun toDomain(): MODEL
-}
-
-fun <MODEL> List<DTOMapper<MODEL>>.toDomain(): List<MODEL> {
-    return map { it.toDomain() }
-}

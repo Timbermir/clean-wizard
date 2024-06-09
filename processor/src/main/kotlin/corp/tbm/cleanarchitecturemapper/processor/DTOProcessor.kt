75f7a906f7729d99664b3db91d34a975bb8a2529
timplifier <timplifier@gmail.com>
2024-06-09 21:51:55 +0600

    feat(processor/DTOProcessor.kt): mature optimization

--- processor/src/main/kotlin/corp/tbm/cleanarchitecturemapper/processor/DTOProcessor.kt
+++ processor/src/main/kotlin/corp/tbm/cleanarchitecturemapper/processor/DTOProcessor.kt
@@ -26,8 +26,8 @@ import corp.tbm.cleanarchitecturemapper.visitors.enums.EnumGenerateVisitor
 import kotlinx.serialization.SerialName
 import java.io.OutputStreamWriter
 
-const val PARAMETER_SEPARATOR = ", \n"
-const val PARAMETER_PREFIX = "\n"
+const val PARAMETER_SEPARATOR = ", \n    "
+const val PARAMETER_PREFIX = "\n    "
 
 class DTOProcessor(
     private val codeGenerator: CodeGenerator,
@@ -50,8 +50,8 @@ class DTOProcessor(
             "return %T(${
                 properties.map { it }
                     .joinToString(
-                        separator = "$PARAMETER_SEPARATOR    ",
-                        prefix = "$PARAMETER_PREFIX    "
+                        separator = PARAMETER_SEPARATOR,
+                        prefix = PARAMETER_PREFIX
                     )
                     { currentProperty ->
                         val filteredProperties = properties.filter { it.name == currentProperty.name }
@@ -132,10 +132,8 @@ class DTOProcessor(
             ) {
                 if (processingRound == 1) {
                     symbol.getDeclaredProperties().forEach { property ->
-                        val packageName = "${
-                            symbol.packagePath.split(".").dropLast(1)
-                                .joinToString(".") + "." + symbol.name.replace(dtoRegex, "")
-                                .firstCharLowercase()
+                        val packageName = "${symbol.basePackagePath}.${
+                            symbol.name.replace(dtoRegex, "").firstCharLowercase()
                         }.${domainOptions.packageName}"
 
                         val propertyAnnotations = property.annotations.filter { it.name.endsWith("Enum") }.toList()
@@ -305,8 +303,8 @@ class DTOProcessor(
         statementFormat: String = "return %T(${
             properties.map { it.name }
                 .joinToString(
-                    separator = "$PARAMETER_SEPARATOR    ",
-                    prefix = "$PARAMETER_PREFIX    "
+                    separator = PARAMETER_SEPARATOR,
+                    prefix = PARAMETER_PREFIX
                 )
                 { propertyName ->
                     if (properties.filter { it.name == propertyName }
@@ -338,22 +336,20 @@ class DTOProcessor(
 
         val className = symbol.name.replace(dtoRegex, "") + classGenerationConfig.suffix
 
-        val packageName = "${
-            symbol.packagePath.split(".").dropLast(1)
-                .joinToString(".") + "." + symbol.name.replace(dtoRegex, "").firstCharLowercase()
+        val packageName = "${symbol.basePackagePath}.${
+            symbol.name.replace(dtoRegex, "").firstCharLowercase()
         }.${classGenerationConfig.packageName}"
 
         val classToBuild = classBuilder(
             TypeSpec.classBuilder(className)
                 .addModifiers(KModifier.DATA)
-
                 .primaryConstructor(
                     FunSpec.constructorBuilder().apply {
                         properties.forEach { property ->
                             addParameter(
                                 ParameterSpec.builder(
                                     property.name,
-                                    property.determineParameterType(symbol, resolver, packageName, logger)
+                                    property.determineParameterType(symbol, resolver, packageName)
                                 ).also {
                                     if (property.type.resolve().isMarkedNullable)
                                         it.defaultValue("%S", null)
@@ -367,7 +363,7 @@ class DTOProcessor(
 
                         PropertySpec.builder(
                             property.name,
-                            property.determineParameterType(symbol, resolver, packageName, logger)
+                            property.determineParameterType(symbol, resolver, packageName)
                         ).also {
                             it.mutable(property.isMutable)
                             it.addModifiers(property.modifiers.toList().map { modifier -> modifier.toKModifier() }
@@ -401,10 +397,9 @@ class DTOProcessor(
                 className
             )
 
-            if (!resolver.getNewFiles().any { it.filePath in codeGenerator.generatedFile.map { it.path } })
-                OutputStreamWriter(file).use { writer ->
-                    fileSpec.writeTo(writer)
-                }
+            OutputStreamWriter(file).use { writer ->
+                fileSpec.writeTo(writer)
+            }
         } catch (fileAlreadyExists: FileAlreadyExistsException) {
         }
     }

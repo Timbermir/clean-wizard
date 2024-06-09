e4838d6340c9d266ef4df47d3f7ed728d65f247a
timplifier <timplifier@gmail.com>
2024-06-09 14:36:33 +0600

    feat(build.gradle.kts): added clean-wizard plugin; added clean-wizard extension block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,29 @@
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import corp.tbm.cleanarchitecturemapper.buildLogic.convention.foundation.CleanWizardJsonSerializer
 
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.cleanwizard)
 }
 
 buildscript {
     dependencies {
         classpath(libs.kotlin.gradle.plugin)
     }
+}
+
+`clean-wizard` {
+    dtoClassSuffix = "Dto"
+    dtoClassPackageName = "dtos"
+    dtoInterfaceMapperName = "DtoMapper"
+    dtoToDomainMapFunctionName = "toModel"
+    domainToDtoMapFunctionName = "fromDomain"
+    domainClassSuffix = "Domain"
+    domainClassPackageName = "models"
+    uiClassSuffix = "Ui"
+    uiClassPackageName = "uis"
+    domainToUiMapFunctionName = "toUI"
+    uiToDomainMapFunctionName = "fromUI"
+    defaultJsonSerializer = CleanWizardJsonSerializer.KOTLINX_SERIALIZATION
 }

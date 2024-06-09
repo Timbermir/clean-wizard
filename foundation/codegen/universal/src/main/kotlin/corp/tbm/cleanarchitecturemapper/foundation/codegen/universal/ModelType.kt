ce5a60a0214ccb856d27d6dded44bf0030073ba8
timplifier <timplifier@gmail.com>
2024-06-09 14:39:23 +0600

    feat(foundation/codegen/universal/ModelType.kt): added package name

--- foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/ModelType.kt
+++ foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/ModelType.kt
@@ -1,9 +1,11 @@
 package corp.tbm.cleanarchitecturemapper.foundation.codegen.universal
 
-import corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.processor.ProcessorOptions
+import corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.processor.ProcessorOptions.domainOptions
+import corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.processor.ProcessorOptions.dtoOptions
+import corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.processor.ProcessorOptions.uiOptions
 
-enum class ModelType(val suffix: String) {
-    DTO(ProcessorOptions.dtoOptions.suffix),
-    MODEL(ProcessorOptions.domainOptions.suffix),
-    UI(ProcessorOptions.uiOptions.suffix)
+enum class ModelType(val suffix: String, val packageName: String) {
+    DTO(dtoOptions.suffix, dtoOptions.packageName),
+    MODEL(domainOptions.suffix, domainOptions.packageName),
+    UI(uiOptions.suffix, uiOptions.packageName);
 }

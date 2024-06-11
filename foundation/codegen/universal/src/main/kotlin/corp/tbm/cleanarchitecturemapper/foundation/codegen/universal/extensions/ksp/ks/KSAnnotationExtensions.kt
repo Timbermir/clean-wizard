04a18eef6f51ada6e3cacc0899ba594783768aba
timplifier <timplifier@gmail.com>
2024-06-12 00:36:31 +0600

    feat(ksp/ks/KSAnnotationExtensions.kt): inline property to check if it is enum

--- foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/extensions/ksp/ks/KSAnnotationExtensions.kt
+++ foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanarchitecturemapper/foundation/codegen/universal/extensions/ksp/ks/KSAnnotationExtensions.kt
@@ -1,6 +1,10 @@
 package corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.extensions.ksp.ks
 
 import com.google.devtools.ksp.symbol.KSAnnotation
+import corp.tbm.cleanarchitecturemapper.foundation.codegen.universal.EnumType
 
 inline val KSAnnotation.name
     get() = shortName.asString()
+
+inline val KSAnnotation.isEnum
+    get() = annotationType.resolve().declaration.fullyQualifiedName in EnumType.entries.map { it.annotation.qualifiedName }

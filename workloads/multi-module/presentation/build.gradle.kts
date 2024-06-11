ba5f473d1f3a9106efc00a60f9e7c817fbb275ad
timplifier <timplifier@gmail.com>
2024-06-12 00:40:44 +0600

    refactor(workloads/multi-module/presentation/build.gradle.kts): cleaned

--- workloads/multi-module/presentation/build.gradle.kts
+++ workloads/multi-module/presentation/build.gradle.kts
@@ -5,7 +5,3 @@ dependencies {
     implementation(projects.workloads.multiModule.data)
     implementation(projects.workloads.multiModule.domain)
 }
-
-sourceSets.main {
-    kotlin.srcDir("data/build/generated/ksp/main/kotlin")
-}

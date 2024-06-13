ffa8a123724079a5fb84056549f5ab81cc1c866f
Timur Abdyraev <timplifier@gmail.com>
2024-06-13 17:28:22 +0600

    feat(workloads/multi-module/data/build.gradle.kts): added logic to extract base package path

--- workloads/multi-module/data/build.gradle.kts
+++ workloads/multi-module/data/build.gradle.kts
@@ -9,6 +9,10 @@ dependencies {
     implementation(projects.workloads.multiModule.domain)
 }
 
+tasks.withType<KspTask>().configureEach {
+    finalizedBy("copyGeneratedDomainClasses")
+}
+
 tasks.register<Copy>("copyGeneratedDomainClasses") {
 
     from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
@@ -46,8 +50,49 @@ tasks.register<Delete>("cleanDomainAndPresentationClassesInData") {
     delete(fileTree)
     delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/${cleanWizardProcessorConfig.domainModuleName}")
     delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/${cleanWizardProcessorConfig.presentationModuleName}")
+    finalizedBy("findBasePackage")
 }
 
-tasks.withType<KspTask>().configureEach {
-    finalizedBy("copyGeneratedDomainClasses")
+tasks.register("findBasePackage") {
+    doLast {
+        sourceSets["main"].allSource.srcDirs.forEach { dir ->
+            println("Searching in source directory: $dir")
+            findBasePackageInDir(dir)
+        }
+    }
+}
+
+fun findBasePackageInDir(dir: File) {
+
+    val findSubdirectories: (File) -> List<File> = { baseDir ->
+        baseDir.listFiles()?.filter { it.isDirectory } ?: emptyList()
+    }
+
+    val findClassFiles: (File) -> List<File> = { baseDir ->
+        baseDir.listFiles()?.filter { it.name.endsWith(".kt") || it.name.endsWith(".java") } ?: emptyList()
+    }
+
+    val basePackagePath: String
+    var currentDir = dir
+    while (true) {
+        val subdirs = findSubdirectories(currentDir)
+        val classFiles = findClassFiles(currentDir)
+
+        if (classFiles.isNotEmpty() || subdirs.isEmpty()) {
+            basePackagePath = currentDir.absolutePath.replace(dir.absolutePath, "")
+            break
+        }
+
+        if (subdirs.size == 1) {
+            currentDir = subdirs[0]
+        } else {
+            basePackagePath = currentDir.absolutePath.replace(dir.absolutePath, "")
+            break
+        }
+    }
+
+    if (basePackagePath.isNotEmpty()) {
+        cleanWizardProcessorConfig.basePackagePath =
+            if (basePackagePath.startsWith(File.separator)) basePackagePath.substring(1) else basePackagePath
+    }
 }

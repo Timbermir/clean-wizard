f6c0f2679b828a4b20d34f844b74a09ef1834517
Timur Abdyraev <timplifier@gmail.com>
2024-06-13 14:15:46 +0600

    feat(multi-module/data/build.gradle.kts): finally done with generating for multi-module; optimized

--- workloads/multi-module/data/build.gradle.kts
+++ workloads/multi-module/data/build.gradle.kts
@@ -1,65 +1,53 @@
 import com.google.devtools.ksp.gradle.KspTask
-import corp.tbm.cleanwizard.buildLogic.convention.foundation.CleanWizardProcessorConfig
+import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.cleanWizardProcessorConfig
 
 plugins {
     alias(libs.plugins.cleanwizard.workload)
 }
+
 dependencies {
     implementation(projects.workloads.multiModule.domain)
 }
 
-tasks.register<Copy>("moveGeneratedFiles") {
-    dependsOn("kspKotlin")
+tasks.register<Copy>("copyGeneratedDomainClasses") {
 
-    rootProject.extensions.findByType(CleanWizardProcessorConfig::class.java)?.let { cleanWizardConfig ->
-        from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
-            exclude("**/${cleanWizardConfig.dataModuleName}/**")
-            exclude("**/${cleanWizardConfig.presentationModuleName}/**")
-        }
-
-        include("**/*.kt")
-        includeEmptyDirs = false
-
-        into("${projects.workloads.multiModule.domain.dependencyProject.layout.buildDirectory.asFile.get().path}/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
+    from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
+        exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
+        exclude("**/${cleanWizardProcessorConfig.presentationModuleName}/**")
     }
-    finalizedBy("copyGeneratedUIClassesToPresentation")
+
+    include("**/*.kt")
+    includeEmptyDirs = false
+
+    into("${projects.workloads.multiModule.domain.dependencyProject.layout.buildDirectory.asFile.get().path}/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
+    finalizedBy("copyGeneratedUIClasses")
 }
 
-tasks.register<Copy>("copyGeneratedUIClassesToPresentation") {
-    dependsOn("moveGeneratedFiles")
-    rootProject.extensions.findByType(CleanWizardProcessorConfig::class.java)?.let { cleanWizardProcessorConfig ->
-        from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
-            exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
-            exclude("**/${cleanWizardProcessorConfig.domainModuleName}/**")
-
-            include("**/*.kt")
-            includeEmptyDirs = false
-
-        }
-
-        into("${projects.workloads.multiModule.presentation.dependencyProject.layout.buildDirectory.asFile.get().path}/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
+tasks.register<Copy>("copyGeneratedUIClasses") {
+    from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
+        exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
+        exclude("**/${cleanWizardProcessorConfig.domainModuleName}/**")
     }
+    include("**/*.kt")
+    includeEmptyDirs = false
+
+    into("${projects.workloads.multiModule.presentation.dependencyProject.layout.buildDirectory.asFile.get().path}/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
     finalizedBy("cleanDomainAndPresentationClassesInData")
 }
 
 tasks.register<Delete>("cleanDomainAndPresentationClassesInData") {
-    rootProject.extensions.findByType(CleanWizardProcessorConfig::class.java)?.let { config ->
-        val dataDir = file("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
-        val fileTree = fileTree(dataDir) {
-            this.exclude()
-            exclude("**/${config.dataModuleName}/**")
-            include("**/*.kt")
-        }
-        delete(fileTree)
+
+    val dataDir = file("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
+    val fileTree = fileTree(dataDir) {
+        exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
+        include("**/*.kt")
     }
-    delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/domain")
-    delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/presentation")
+
+    delete(fileTree)
+    delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/${cleanWizardProcessorConfig.domainModuleName}")
+    delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/${cleanWizardProcessorConfig.presentationModuleName}")
 }
 
 tasks.withType<KspTask>().configureEach {
-    finalizedBy("moveGeneratedFiles")
-}
-
-tasks.named("compileKotlin") {
-    dependsOn("moveGeneratedFiles")
+    finalizedBy("copyGeneratedDomainClasses")
 }

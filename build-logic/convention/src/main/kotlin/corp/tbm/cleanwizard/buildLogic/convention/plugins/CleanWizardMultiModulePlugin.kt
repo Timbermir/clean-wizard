d643a08c8065fb83437ea0288b8966ed56eb1b6f
Timur Abdyraev <timplifier@gmail.com>
2024-06-14 16:29:13 +0600

    feat(convention/plugins/CleanWizardMultiModulePlugin.kt): completed

--- build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/plugins/CleanWizardMultiModulePlugin.kt
+++ build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/plugins/CleanWizardMultiModulePlugin.kt
@@ -1,65 +1,177 @@
 package corp.tbm.cleanwizard.buildLogic.convention.plugins
 
-import com.google.devtools.ksp.gradle.KspTask
-import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.cleanWizardProcessorConfig
-import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.libs
+import com.google.devtools.ksp.gradle.KspGradleSubplugin
+import com.google.devtools.ksp.gradle.KspTaskJvm
+import corp.tbm.cleanwizard.buildLogic.convention.foundation.CleanWizardCodegenExtension
+import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.CopySpec
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.Delete
-import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.project
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.File
 
 class CleanWizardMultiModulePlugin : Plugin<Project> {
+
+    private var lastPackageSegmentWhereFirstSourceClassOccurs = ""
     override fun apply(target: Project) {
+        var configured = false
         with(target) {
-//            pluginManager.withPlugin(libs.plugins.google.devtools.ksp.get().pluginId) {
-//                afterEvaluate {
-//                    tasks.register<Copy>("copyGeneratedDomainClasses") {
-//
-//                        from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
-//                            exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
-//                            exclude("**/${cleanWizardProcessorConfig.presentationModuleName}/**")
-//                        }
-//
-//                        include("**/*.kt")
-//                        includeEmptyDirs = false
-//
-//                        into("${projects.workloads.multiModule.domain.dependencyProject.layout.buildDirectory.asFile.get().path}/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
-//                        finalizedBy("copyGeneratedUIClasses")
-//                    }
-//
-//                    tasks.register<Copy>("copyGeneratedUIClasses") {
-//                        from("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule") {
-//                            exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
-//                            exclude("**/${cleanWizardProcessorConfig.domainModuleName}/**")
-//                        }
-//                        include("**/*.kt")
-//                        includeEmptyDirs = false
-//
-//                        into("${projects.workloads.multiModule.presentation.dependencyProject.layout.buildDirectory.asFile.get().path}/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
-//                        finalizedBy("cleanDomainAndPresentationClassesInData")
-//                    }
-//
-//                    tasks.register<Delete>("cleanDomainAndPresentationClassesInData") {
-//
-//                        val dataDir = file("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule")
-//                        val fileTree = fileTree(dataDir) {
-//                            exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
-//                            include("**/*.kt")
-//                        }
-//
-//                        delete(fileTree)
-//                        delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/${cleanWizardProcessorConfig.domainModuleName}")
-//                        delete("build/generated/ksp/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/${cleanWizardProcessorConfig.presentationModuleName}")
-//                    }
-//
-//                    tasks.withType<KspTask>().configureEach {
-//                        finalizedBy("copyGeneratedDomainClasses")
-//                    }
-//                }
-//            }
+
+            alias(libs.plugins.cleanwizard.kotlin)
+            alias(libs.plugins.google.devtools.ksp)
+
+            val codegenExtension =
+                extensions.create("clean-wizard-codegen", CleanWizardCodegenExtension::class.java)
+
+            dependencies {
+                implementation(libs.bundles.kotlinx)
+                implementation(project(":foundation:annotations"))
+                implementation(project(":clean-wizard"))
+                ksp(project(":processor"))
+            }
+
+            plugins.withType(KspGradleSubplugin::class.java) {
+                configured = true
+            }
+
+            project.afterEvaluate {
+                check(configured) {
+                    "The KSP Gradle Plugin is required in order to run plugin"
+                }
+            }
+
+            sourceSets {
+                getByName("main") {
+                    kotlin {
+                        srcDirs.forEach { dir ->
+                            findBasePackageInDir(dir)
+                        }
+                    }
+                }
+            }
+
+            tasks.named("compileKotlin") {
+                dependsOn(project(":workloads:multi-module:data").tasks.named("copyGeneratedUIClasses"))
+            }
+
+            tasks.withType<KotlinCompile>().configureEach {
+                compilerOptions {
+                    jvmTarget.set(this@with.jvmTarget)
+                }
+            }
+
+            tasks.withType<KspTaskJvm>().configureEach {
+                finalizedBy("copyGeneratedDomainClasses")
+            }
+
+            tasks.register<Copy>("copyGeneratedDomainClasses") {
+
+                copyToModule(
+                    this,
+                    "kspKotlin",
+                    {
+                        exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
+                        exclude("**/${cleanWizardProcessorConfig.presentationModuleName}/**")
+                    },
+                    project(codegenExtension.domainProject),
+                    "copyGeneratedUIClasses"
+                )
+            }
+
+            tasks.register<Copy>("copyGeneratedUIClasses") {
+                copyToModule(
+                    this,
+                    "copyGeneratedDomainClasses",
+                    {
+                        exclude("**/${cleanWizardProcessorConfig.dataModuleName}/**")
+                        exclude("**/${cleanWizardProcessorConfig.domainModuleName}/**")
+                    },
+                    project(codegenExtension.presentationProject),
+                    "cleanDomainAndPresentationClassesInData"
+                )
+            }
+
+            tasks.register<Delete>("cleanDomainAndPresentationClassesInData") {
+                dependsOn("copyGeneratedUIClasses")
+
+                val basePackage = File(
+                    kspMainBuildDirectory,
+                    lastPackageSegmentWhereFirstSourceClassOccurs.split(".").joinToString("/")
+                        .replace(cleanWizardProcessorConfig.dataModuleName, "")
+                ).path
+
+                delete(File(basePackage, "/${cleanWizardProcessorConfig.domainModuleName}"))
+                delete(File(basePackage, "/${cleanWizardProcessorConfig.presentationModuleName}"))
+            }
         }
     }
+
+    private fun configureGradleTasks() {
+
+    }
+
+    private fun Project.copyToModule(
+        copy: Copy,
+        taskToDependOn: String,
+        fromCopySpec: CopySpec.() -> Unit,
+        pathToCopyInto: Project,
+        taskToFinalizeBy: String
+    ) {
+        copy.apply {
+            dependsOn(taskToDependOn)
+
+            from(kspMainBuildDirectory) {
+                fromCopySpec()
+            }
+
+            include("**/*.kt")
+            includeEmptyDirs = false
+
+            into(pathToCopyInto.kspMainBuildDirectory)
+            finalizedBy(taskToFinalizeBy)
+        }
+    }
+
+    private fun findBasePackageInDir(dir: File) {
+
+        val findSubdirectories: (File) -> List<File> = { baseDir ->
+            baseDir.listFiles()?.filter { it.isDirectory } ?: emptyList()
+        }
+
+        val findClassFiles: (File) -> List<File> = { baseDir ->
+            baseDir.listFiles()?.filter { it.name.endsWith(".kt") || it.name.endsWith(".java") }
+                ?: emptyList()
+        }
+
+        val basePackagePath: String
+        var currentDir = dir
+        while (true) {
+            val subdirs = findSubdirectories(currentDir)
+            val classFiles = findClassFiles(currentDir)
+
+            if (classFiles.isNotEmpty() || subdirs.isEmpty()) {
+                basePackagePath = currentDir.absolutePath.replace(dir.absolutePath, "")
+                break
+            }
+
+            if (subdirs.size == 1) {
+                currentDir = subdirs[0]
+            } else {
+                basePackagePath = currentDir.absolutePath.replace(dir.absolutePath, "")
+                break
+            }
+        }
+
+        if (basePackagePath.isNotEmpty()) {
+            lastPackageSegmentWhereFirstSourceClassOccurs =
+                if (basePackagePath.startsWith(File.separator)) basePackagePath.substring(1) else basePackagePath
+        }
+    }
+
 }

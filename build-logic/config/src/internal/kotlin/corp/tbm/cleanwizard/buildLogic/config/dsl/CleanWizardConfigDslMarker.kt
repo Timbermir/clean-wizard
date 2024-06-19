cd19ba3249a006ec83a28c77c6b7e3f926d67a0e
Timur Abdyraev <timplifier@gmail.com>
2024-06-19 15:46:29 +0600

    refactor(config/dsl/CleanWizardConfigDslMarker.kt): renamed to CleanWizardConfigDsl.kt

--- build-logic/config/src/internal/kotlin/corp/tbm/cleanwizard/buildLogic/config/dsl/CleanWizardConfigDslMarker.kt
+++
@@ -1,6 +0,0 @@
-package corp.tbm.cleanwizard.buildLogic.config.dsl
-
-@DslMarker
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.BINARY)
-internal annotation class CleanWizardConfigDslMarker

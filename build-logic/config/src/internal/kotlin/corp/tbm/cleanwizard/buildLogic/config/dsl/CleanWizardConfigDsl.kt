730e97c78fce6ecbde32d5650b06512d6cef3b8b
timplifier <timplifier@gmail.com>
2024-06-19 21:26:09 +0600

    refactor(config/dsl/CleanWizardConfigDsl.kt): deleted @Retention

--- build-logic/config/src/internal/kotlin/corp/tbm/cleanwizard/buildLogic/config/dsl/CleanWizardConfigDsl.kt
+++ build-logic/config/src/internal/kotlin/corp/tbm/cleanwizard/buildLogic/config/dsl/CleanWizardConfigDsl.kt
@@ -1,6 +1,6 @@
 package corp.tbm.cleanwizard.buildLogic.config.dsl
 
+
 @DslMarker
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
-@Retention(AnnotationRetention.BINARY)
 annotation class CleanWizardConfigDsl

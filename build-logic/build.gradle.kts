d284585d4b49927b3520d5dec85647e8f3dadf31
Timur Abdyraev <timplifier@gmail.com>
2024-06-19 17:11:11 +0600

    refactor(build-logic/build.gradle.kts): deleted jvm plugin; added dsl

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,14 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    `kotlin-dsl`
-    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.dsl) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-}
-
-buildscript {
-    dependencies {
-        classpath(libs.kotlin.gradle.plugin)
-    }
 }

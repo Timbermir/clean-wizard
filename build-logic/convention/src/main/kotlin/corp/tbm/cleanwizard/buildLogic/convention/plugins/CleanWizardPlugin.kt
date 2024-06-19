a936d01ba18b62a1651ba81db561887fb2675255
Timur Abdyraev <timplifier@gmail.com>
2024-06-19 16:16:09 +0600

    refactor(convention/plugins/CleanWizardPlugin.kt): deleted three layers; replaced with wrapper

--- build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/plugins/CleanWizardPlugin.kt
+++ build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/plugins/CleanWizardPlugin.kt
@@ -1,21 +1,12 @@
 package corp.tbm.cleanwizard.buildLogic.convention.plugins
 
-//import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.cleanWizardProcessorConfig
-//import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.ksp
-//import corp.tbm.cleanwizard.buildLogic.convention.processorConfig.CleanWizardProcessorConfig
-//import kotlinx.serialization.encodeToString
 import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.cleanWizardExtension
 import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.createTempFileWithEncodedString
+import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.json
 import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.ksp
 import corp.tbm.cleanwizard.buildLogic.convention.plugins.extensions.CleanWizardExtension
-import kotlinx.serialization.json.Json
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-val json = Json {
-    classDiscriminator = "type"
-    encodeDefaults = true
-}
 
 internal class CleanWizardPlugin : Plugin<Project> {
 
@@ -41,20 +32,10 @@ internal class CleanWizardPlugin : Plugin<Project> {
                                 )
                                 arg(
                                     "LAYER_CONFIGS",
-                                    json.createTempFileWithEncodedString(cleanWizardLayerConfigWrapper).absolutePath
+                                    json.createTempFileWithEncodedString(layerConfigs).absolutePath
                                 )
-                                arg(
-                                    "DATA_CONFIG",
-                                    json.createTempFileWithEncodedString(dataConfig).absolutePath
-                                )
-                                arg(
-                                    "DOMAIN_CONFIG",
-                                    json.createTempFileWithEncodedString(domainConfig).absolutePath
-                                )
-                                arg(
-                                    "PRESENTATION_CONFIG",
-                                    json.createTempFileWithEncodedString(presentationConfig).absolutePath
-                                )
+                                println(dependencyInjectionFramework)
+                                println(dependencyInjectionFramework.dependencies)
                             }
                         }
                     }

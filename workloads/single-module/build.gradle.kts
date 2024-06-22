3b885fd130a7121eeab21eb12d71c5c4e42cdcf1
timplifier <timplifier@gmail.com>
2024-06-22 22:26:15 +0600

    feat(workloads/single-module/build.gradle.kts): enabled proper workload for use case gen

--- workloads/single-module/build.gradle.kts
+++ workloads/single-module/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 dependencies {
     implementation(projects.foundation.annotations)
     implementation(projects.cleanWizard)
+    implementation(libs.bundles.di)
     ksp(projects.processors.dataClass)
     ksp(projects.processors.useCase)
+    ksp(libs.koin.annotations.ksp)
 }

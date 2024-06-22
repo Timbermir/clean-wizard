04c77545351054bf3a83ee230f9a952f9d1fd0d6
timplifier <timplifier@gmail.com>
2024-06-22 21:18:57 +0600

    feat(extensions/kotlinpoet/FileSpecExtensions.kt): added extension to add imports

--- foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanwizard/foundation/codegen/universal/extensions/kotlinpoet/FileSpecExtensions.kt
+++ foundation/codegen/universal/src/main/kotlin/corp/tbm/cleanwizard/foundation/codegen/universal/extensions/kotlinpoet/FileSpecExtensions.kt
@@ -3,6 +3,7 @@ package corp.tbm.cleanwizard.foundation.codegen.universal.extensions.kotlinpoet
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.Dependencies
 import com.squareup.kotlinpoet.FileSpec
+import corp.tbm.cleanwizard.buildLogic.config.CleanWizardImport
 import java.io.OutputStreamWriter
 
 fun FileSpec.writeNewFile(codeGenerator: CodeGenerator, dependencies: Dependencies = Dependencies.ALL_FILES) {
@@ -10,4 +11,8 @@ fun FileSpec.writeNewFile(codeGenerator: CodeGenerator, dependencies: Dependenci
     OutputStreamWriter(file).use { writer ->
         writeTo(writer)
     }
+}
+
+fun FileSpec.Builder.addImport(cleanWizardImport: CleanWizardImport) {
+    addImport(cleanWizardImport.packageName, cleanWizardImport.name)
 }

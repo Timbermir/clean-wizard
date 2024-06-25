7ea57762b75c3c524bb7ff30519f274f2b593105
Timur Abdyraev <timplifier@gmail.com>
2024-06-25 14:54:48 +0600

    feat(workloads/single-module/data): added the Rick And Morty workload

---
+++ workloads/single-module/src/main/kotlin/corp/tbm/cleanwizard/workloads/singlemodule/data/dtos/rickAndMorty/EpisodeDTOSchema.kt
@@ -0,0 +1,18 @@
+package corp.tbm.cleanwizard.workloads.singlemodule.data.dtos.rickAndMorty
+
+import corp.tbm.cleanwizard.foundation.annotations.BackwardsMappingConfig
+import corp.tbm.cleanwizard.foundation.annotations.DTO
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.serialization.SerialName
+
+@DTO(false, backwardsMappingConfig = BackwardsMappingConfig.FULL_MAPPING)
+data class EpisodeDTOSchema(
+    val id: Int,
+    val name: String,
+    @SerialName("air_date")
+    val airDate: String,
+    val episode: String,
+    val characters: ImmutableList<String>,
+    val url: String,
+    val created: String
+)

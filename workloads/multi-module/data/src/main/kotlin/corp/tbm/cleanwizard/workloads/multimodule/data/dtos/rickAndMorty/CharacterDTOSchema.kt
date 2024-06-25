4da71c41db36a535489a4e7c680ecff982b0d0d7
Timur Abdyraev <timplifier@gmail.com>
2024-06-25 14:55:07 +0600

    feat(workloads/multi-module/data): added the Rick And Morty workload

---
+++ workloads/multi-module/data/src/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/data/dtos/rickAndMorty/CharacterDTOSchema.kt
@@ -0,0 +1,26 @@
+package corp.tbm.cleanwizard.workloads.multimodule.data.dtos.rickAndMorty
+
+import corp.tbm.cleanwizard.foundation.annotations.BackwardsMappingConfig
+import corp.tbm.cleanwizard.foundation.annotations.DTO
+import corp.tbm.cleanwizard.foundation.annotations.StringEnum
+
+@DTO(false, backwardsMappingConfig = BackwardsMappingConfig.FULL_MAPPING)
+data class CharacterDTOSchema(
+    val id: Int,
+    val name: String,
+    @StringEnum(enumEntries = ["Alive", "Dead", "Unknown"], enumEntryValues = ["Alive", "Dead", "unknown"])
+    val status: String,
+    @StringEnum(
+        enumEntries = ["Human", "Humanoid", "Alien", "Unknown", "Mythological_Creature"],
+        enumEntryValues = ["Human", "Humanoid", "Alien", "Unknown", "Mythological Creature"]
+    )
+    val species: String,
+    @StringEnum(enumEntries = ["Male", "Female", "Unknown"], enumEntryValues = ["Male", "Female", "unknown"])
+    val gender: String,
+    val origin: OriginDTOSchema,
+    val location: OriginDTOSchema,
+    val image: String,
+    val episodes: List<String>,
+    val url: String,
+    val created: String
+)
---
+++ workloads/multi-module/data/src/main/kotlin/corp/tbm/cleanwizard/workloads/multimodule/data/dtos/rickAndMorty/LocationDTOSchema.kt
@@ -0,0 +1,21 @@
+package corp.tbm.cleanwizard.workloads.multimodule.data.dtos.rickAndMorty
+
+import corp.tbm.cleanwizard.foundation.annotations.BackwardsMappingConfig
+import corp.tbm.cleanwizard.foundation.annotations.DTO
+import corp.tbm.cleanwizard.foundation.annotations.StringEnum
+import kotlinx.collections.immutable.ImmutableList
+
+@DTO(false, backwardsMappingConfig = BackwardsMappingConfig.FULL_MAPPING)
+data class LocationDTOSchema(
+    val id: Int,
+    val name: String,
+    @StringEnum(
+        enumEntries = ["Planet", "Space Station", "Cluster", "Microverse", "Resort", "TV", "Fantasy", "Dream"],
+        enumEntryValues = ["Planet", "Space Station", "Cluster", "Microverse", "Resort", "TV", "Fantasy", "Dream"]
+    )
+    val type: String,
+    val dimension: String,
+    val residents: ImmutableList<String>,
+    val url: String,
+    val created: String
+)

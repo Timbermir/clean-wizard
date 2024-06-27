1e5ef71f2b5ae55da8c8cd1cf669653ec7bedaca
timplifier <timplifier@gmail.com>
2024-06-27 12:04:27 +0600

    refactor(workloads/multi-module/data/build.gradle.kts): included more deps and plugins

--- workloads/multi-module/data/build.gradle.kts
+++ workloads/multi-module/data/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
+    alias(libs.plugins.cleanwizard.internal.kotlin)
     alias(libs.plugins.cleanwizard.multimodule)
 }
 
 dependencies {
     implementation(projects.workloads.multiModule.domain)
+    implementation(libs.bundles.kotlinx)
     implementation(libs.kodein)
 }
 

fc89e6ba749db5f6f848711e46e3f4b65f550aab
timplifier <timplifier@gmail.com>
2024-06-30 22:44:46 +0600

    style(CleanWizardMultiModuleCodegenExtension.kt): styled

--- build-logic/config/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/config/api/CleanWizardMultiModuleCodegenExtension.kt
+++ build-logic/config/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/config/api/CleanWizardMultiModuleCodegenExtension.kt
@@ -1,6 +1,6 @@
 package corp.tbm.cleanwizard.buildLogic.config.api
 
-abstract class CleanWizardMultiModuleCodegenExtension{
+abstract class CleanWizardMultiModuleCodegenExtension {
     abstract var domainProjectPath: String
     abstract var presentationProjectPath: String
 }

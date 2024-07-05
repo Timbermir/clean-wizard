bfb5b05971b4c4fe5606fadbeb3602d3fffc47a1
Timur Abdyraev <timplifier@gmail.com>
2024-07-05 18:43:06 +0600

    feat(buildLogic/convention/implementation/serializers/*): moved

---
+++ build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/implementation/serializers/CleanWizardKotlinXSerializationBuilderImplementation.kt
@@ -0,0 +1,50 @@
+package corp.tbm.cleanwizard.buildLogic.convention.implementation.serializers
+
+import corp.tbm.cleanwizard.buildLogic.config.CleanWizardJsonSerializer
+import corp.tbm.cleanwizard.buildLogic.config.CleanWizardKotlinXSerializationConfig
+import corp.tbm.cleanwizard.buildLogic.config.api.CleanWizardKotlinXSerializationBuilder
+import corp.tbm.cleanwizard.buildLogic.config.api.CleanWizardKotlinXSerializationConfigBuilder
+import corp.tbm.cleanwizard.buildLogic.config.toCleanWizardKotlinXSerializationJsonNamingStrategy
+import kotlinx.serialization.ExperimentalSerializationApi
+
+internal class CleanWizardKotlinXSerializationBuilderImplementation : CleanWizardKotlinXSerializationBuilder() {
+
+    private val kotlinXSerializationConfigBuilder = CleanWizardKotlinXSerializationConfigBuilderImplementation()
+
+    override fun json(block: CleanWizardKotlinXSerializationConfigBuilder.() -> Unit) {
+        kotlinXSerializationConfigBuilder.apply(block)
+    }
+
+    internal fun build(): CleanWizardJsonSerializer.KotlinXSerialization {
+        return CleanWizardJsonSerializer.KotlinXSerialization(
+            kotlinXSerializationConfigBuilder.build()
+        )
+    }
+
+    private class CleanWizardKotlinXSerializationConfigBuilderImplementation :
+        CleanWizardKotlinXSerializationConfigBuilder() {
+
+        @OptIn(ExperimentalSerializationApi::class)
+        fun build(): CleanWizardKotlinXSerializationConfig {
+            return CleanWizardKotlinXSerializationConfig(
+                encodeDefaults,
+                ignoreUnknownKeys,
+                isLenient,
+                allowStructuredMapKeys,
+                prettyPrint,
+                explicitNulls,
+                prettyPrintIndent,
+                coerceInputValues,
+                useArrayPolymorphism,
+                classDiscriminator,
+                allowSpecialFloatingPointValues,
+                useAlternativeNames,
+                namingStrategy?.toCleanWizardKotlinXSerializationJsonNamingStrategy(),
+                decodeEnumsCaseInsensitive,
+                allowTrailingComma,
+                allowComments,
+                classDiscriminatorMode
+            )
+        }
+    }
+}

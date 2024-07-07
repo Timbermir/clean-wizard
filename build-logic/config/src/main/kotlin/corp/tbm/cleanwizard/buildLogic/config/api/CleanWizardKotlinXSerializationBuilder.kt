c8ae686613dcc8ddf04210f8d3e20c2a548c0411
timplifier <timplifier@gmail.com>
2024-07-07 20:18:56 +0600

    feat(CleanWizardKotlinXSerializationBuilder.kt): added docs

--- build-logic/config/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/config/api/CleanWizardKotlinXSerializationBuilder.kt
+++ build-logic/config/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/config/api/CleanWizardKotlinXSerializationBuilder.kt
@@ -3,33 +3,117 @@ package corp.tbm.cleanwizard.buildLogic.config.api
 import corp.tbm.cleanwizard.buildLogic.config.annotations.CleanWizardConfigDsl
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.ClassDiscriminatorMode
+import kotlinx.serialization.json.JsonBuilder
 import kotlinx.serialization.json.JsonNamingStrategy
 
 @CleanWizardConfigDsl
 abstract class CleanWizardKotlinXSerializationBuilder {
 
     abstract fun json(block: CleanWizardKotlinXSerializationConfigBuilder.() -> Unit)
-}
 
-@CleanWizardConfigDsl
-abstract class CleanWizardKotlinXSerializationConfigBuilder {
-    var encodeDefaults: Boolean = false
-    var ignoreUnknownKeys: Boolean = false
-    var isLenient: Boolean = false
-    var allowStructuredMapKeys: Boolean = false
-    var prettyPrint: Boolean = false
-    var explicitNulls: Boolean = true
-    var prettyPrintIndent: String = "    "
-    var coerceInputValues: Boolean = false
-    var useArrayPolymorphism: Boolean = false
-    var classDiscriminator: String = "type"
-    var allowSpecialFloatingPointValues: Boolean = false
-    var useAlternativeNames: Boolean = true
+    @CleanWizardConfigDsl
+    abstract class CleanWizardKotlinXSerializationConfigBuilder {
+        /**
+         * Similar to [JsonBuilder.encodeDefaults]
+         * @see [JsonBuilder.encodeDefaults]
+         */
+        var encodeDefaults: Boolean = false
 
-    @OptIn(ExperimentalSerializationApi::class)
-    var namingStrategy: JsonNamingStrategy? = null
-    var decodeEnumsCaseInsensitive: Boolean = false
-    var allowTrailingComma: Boolean = false
-    var allowComments: Boolean = false
-    var classDiscriminatorMode: ClassDiscriminatorMode = ClassDiscriminatorMode.POLYMORPHIC
+        /**
+         * Similar to [JsonBuilder.ignoreUnknownKeys]
+         * @see [JsonBuilder.ignoreUnknownKeys]
+         */
+        var ignoreUnknownKeys: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.isLenient]
+         * @see [JsonBuilder.isLenient]
+         */
+        var isLenient: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.allowStructuredMapKeys]
+         * @see [JsonBuilder.allowStructuredMapKeys]
+         */
+        var allowStructuredMapKeys: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.prettyPrint]
+         * @see [JsonBuilder.prettyPrint]
+         */
+        var prettyPrint: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.explicitNulls]
+         * @see [JsonBuilder.explicitNulls]
+         */
+        var explicitNulls: Boolean = true
+
+        /**
+         * Similar to [JsonBuilder.prettyPrintIndent]
+         * @see [JsonBuilder.prettyPrintIndent]
+         */
+        var prettyPrintIndent: String = "    "
+
+        /**
+         * Similar to [JsonBuilder.coerceInputValues]
+         * @see [JsonBuilder.coerceInputValues]
+         */
+        var coerceInputValues: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.useArrayPolymorphism]
+         * @see [JsonBuilder.useArrayPolymorphism]
+         */
+        var useArrayPolymorphism: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.classDiscriminator]
+         * @see [JsonBuilder.classDiscriminator]
+         */
+        var classDiscriminator: String = "type"
+
+        /**
+         * Similar to [JsonBuilder.allowSpecialFloatingPointValues]
+         * @see [JsonBuilder.allowSpecialFloatingPointValues]
+         */
+        var allowSpecialFloatingPointValues: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.useAlternativeNames]
+         * @see [JsonBuilder.useAlternativeNames]
+         */
+        var useAlternativeNames: Boolean = true
+
+        @OptIn(ExperimentalSerializationApi::class)
+                /**
+                 * Similar to [JsonBuilder.namingStrategy]
+                 * @see [JsonBuilder.namingStrategy]
+                 */
+        var namingStrategy: JsonNamingStrategy? = null
+
+        /**
+         * Similar to [JsonBuilder.decodeEnumsCaseInsensitive]
+         * @see [JsonBuilder.decodeEnumsCaseInsensitive]
+         */
+        var decodeEnumsCaseInsensitive: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.allowTrailingComma]
+         * @see [JsonBuilder.allowTrailingComma]
+         */
+        var allowTrailingComma: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.allowComments]
+         * @see [JsonBuilder.allowComments]
+         */
+        var allowComments: Boolean = false
+
+        /**
+         * Similar to [JsonBuilder.classDiscriminatorMode]
+         * @see [JsonBuilder.classDiscriminatorMode]
+         */
+        var classDiscriminatorMode: ClassDiscriminatorMode = ClassDiscriminatorMode.POLYMORPHIC
+    }
 }

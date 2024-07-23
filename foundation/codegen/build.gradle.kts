d6b05fa2f06643d7b707ca062f45cb7fef1ba54f
timplifier <timplifier@gmail.com>
2024-07-24 02:15:24 +0600

    feat(foundation/codegen/build.gradle.kts): added publishing; added gradle-plugins:api project

--- foundation/codegen/build.gradle.kts
+++ foundation/codegen/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     alias(libs.plugins.cleanwizard.internal.codegen)
+    alias(libs.plugins.cleanwizard.internal.publish)
+}
+dependencies {
+    api(libs.cleanwizard.gradle.plugins.api)
 }

fe66259ce546b3688c207721a06e21e38223eae9
timplifier <timplifier@gmail.com>
2024-07-24 02:29:53 +0600

    feat(gradle-plugins/api/build.gradle.kts): api module for plugins

---
+++ gradle-plugins/api/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.cleanwizard.internal.publish)
+}
+
+group = projectConfig.versions.group.get()
+version = projectConfig.versions.version.get()
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.google.gson)
+    implementation(libs.squareup.moshi)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        languageVersion =
+            org.jetbrains.kotlin.gradle.dsl.KotlinVersion.values()
+                .first { it.version == projectConfig.versions.kotlin.get() }
+    }
+}

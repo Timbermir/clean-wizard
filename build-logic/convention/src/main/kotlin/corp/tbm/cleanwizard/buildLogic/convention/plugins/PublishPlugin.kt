623c06c1e23c8020406205cede0a21697bdfa266
timplifier <timplifier@gmail.com>
2024-08-22 10:23:36 +0600

    feat(PublishPlugin.kt): migrating from vanniktech to vanilla maven publish

--- build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/plugins/PublishPlugin.kt
+++ build-logic/convention/src/main/kotlin/corp/tbm/cleanwizard/buildLogic/convention/plugins/PublishPlugin.kt
@@ -1,74 +1,97 @@
 package corp.tbm.cleanwizard.buildLogic.convention.plugins
 
-import com.vanniktech.maven.publish.MavenPublishBaseExtension
-import com.vanniktech.maven.publish.SonatypeHost
 import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.alias
 import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.libs
 import corp.tbm.cleanwizard.buildLogic.convention.foundation.extensions.projectConfig
 import corp.tbm.cleanwizard.buildLogic.convention.plugins.extensions.PublishExtensionImplementation
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.plugins.signing.SigningExtension
 
 internal class PublishPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
 
-            alias(libs.plugins.vanniktech.maven.publish)
+            alias(libs.plugins.maven.publish)
+            alias(libs.plugins.gradle.signing)
 
             val extension = extensions.create("publish", PublishExtensionImplementation::class)
             afterEvaluate {
-                extensions.configure<MavenPublishBaseExtension> {
-                    coordinates(
-                        groupId = projectConfig.versions.group.get(),
-                        artifactId = extension.artifactId.takeIf { it.isNotEmpty() }
-                            ?: project.displayName.replace("project", "").replaceFirst(":", "").replace(":", "-")
-                                .replace("'", "").replace(" ", ""),
-                        version = projectConfig.versions.version.get()
-                    )
-                    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-                    pom {
-                        inceptionYear.set("2024")
-                        name.set("Clean Wizard")
-                        url.set("https://github.com/Timbermir/clean-wizard")
-                        description.set("Clean Wizard - Kotlin Symbol Processor that becomes the wizard you are looking for when dealing with Clean Architecture")
+                extensions.configure<JavaPluginExtension> {
+                    withSourcesJar()
+                    withJavadocJar()
+                }
 
-                        licenses {
-                            license {
-                                name.set("The Apache Software License, Version 2.0")
-                                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                            }
-                        }
+                extensions.configure<PublishingExtension> {
+                    repositories {
+                        mavenCentral()
+                    }
+                    publications {
+                        create<MavenPublication>("mavenPublication") {
+                            from(components["kotlin"])
+                            groupId = projectConfig.versions.group.get()
+                            artifactId = extension.artifactId.takeIf { it.isNotEmpty() }
+                                ?: project.displayName.replace("project", "").replaceFirst(":", "").replace(":", "-")
+                                    .replace("'", "").replace(" ", "")
+                            version = projectConfig.versions.version.get()
+                            pom {
+                                inceptionYear.set("2024")
+                                name.set("Clean Wizard")
+                                url.set("https://github.com/Timbermir/clean-wizard")
+                                description.set("Clean Wizard - Kotlin Symbol Processor that becomes the wizard you are looking for when dealing with Clean Architecture")
 
-                        scm {
-                            url.set("https://github.com/Timbermir/clean-wizard")
-                            connection.set("scm:git:git://github.com/Timbermir/clean-wizard.git")
-                            developerConnection.set("scm:git@github.com:Timbermir/clean-wizard.git")
-                        }
+                                licenses {
+                                    license {
+                                        name.set("The Apache Software License, Version 2.0")
+                                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                                    }
+                                }
 
-                        issueManagement {
-                            system.set("GitHub Issues")
-                            url.set("https://github.com/Timbermir/clean-wizard/issues/")
-                        }
+                                scm {
+                                    url.set("https://github.com/Timbermir/clean-wizard")
+                                    connection.set("scm:git:git://github.com/Timbermir/clean-wizard.git")
+                                    developerConnection.set("scm:git@github.com:Timbermir/clean-wizard.git")
+                                }
 
-                        organization {
-                            name.set("Timbermir")
-                            url.set("https://github.com/Timbermir")
-                        }
+                                issueManagement {
+                                    system.set("GitHub Issues")
+                                    url.set("https://github.com/Timbermir/clean-wizard/issues/")
+                                }
 
-                        developers {
-                            developer {
-                                id.set(findProperty("developerId").toString())
-                                name.set("timplifier")
-                                email.set("timplifier@gmail.com")
-                                url.set("https://github.com/timplifier")
+                                organization {
+                                    name.set("Timbermir")
+                                    url.set("https://github.com/Timbermir")
+                                }
+
+                                developers {
+                                    developer {
+                                        id.set(findProperty("developerId").toString())
+                                        name.set("timplifier")
+                                        email.set("timplifier@gmail.com")
+                                        url.set("https://github.com/timplifier")
+                                    }
+                                }
                             }
                         }
                     }
-                    signAllPublications()
+                }
+
+                extensions.configure<SigningExtension> {
+                    useInMemoryPgpKeys(
+                        findProperty("signing.keyId").toString(),
+                        findProperty("signing.passphrase").toString(),
+                        findProperty("signing.secretKeyRingFile").toString()
+                    )
+                    sign(extensions.getByType<PublishingExtension>().publications["mavenPublication"])
                 }
             }
         }

364ee438f3b71d18e4270852aa1fce64a1a4919d
timplifier <timplifier@gmail.com>
2024-08-22 17:56:31 +0600

    feat(Repository.kt): marked UseCase with @CleanWizardInternalAPI

--- foundation/annotations/src/main/kotlin/corp/tbm/cleanwizard/foundation/annotations/Repository.kt
+++ foundation/annotations/src/main/kotlin/corp/tbm/cleanwizard/foundation/annotations/Repository.kt
@@ -1,9 +1,12 @@
 package corp.tbm.cleanwizard.foundation.annotations
 
+import corp.tbm.cleanwizard.gradle.api.annotations.CleanWizardInternalAPI
+
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class Repository
 
+@CleanWizardInternalAPI
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class UseCase

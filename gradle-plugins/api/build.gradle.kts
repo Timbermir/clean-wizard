50ba1e25b82586283d9e908613c3a1ff18af178d
timplifier <timplifier@gmail.com>
2024-08-22 10:21:07 +0600

    refactor(*): deleted vanniktech dependency

--- gradle-plugins/api/build.gradle.kts
+++ gradle-plugins/api/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.vanniktech.maven.publish)
     alias(libs.plugins.cleanwizard.internal.publish)
 }
 
--- gradle-plugins/build.gradle.kts
+++ gradle-plugins/build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.vanniktech.maven.publish) apply false
 }
